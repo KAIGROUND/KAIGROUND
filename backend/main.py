63904169d047bd3bf48d78ce8759a2dc56fe97a8
dareumHJ <dareum@kaist.ac.kr>
2021-02-15 20:17:27 +0900

    Third

--- backend/main.py
+++ backend/main.py
@@ -6,17 +6,9 @@ class Attitem:
         self.itemimg=itemimage
         self.attpower=attackpower
         self.attdis=attackdistance
-    def setdata(self, name, itemimage, attackpower, attackdistance):
-        self.name=name
-        self.itemimg=itemimage
-        self.attpower=attackpower
-        self.attdis=attackdistance
+
 class Defitem:
     def __init__(self, name, itemimage, defencepower):
-        self.name=name
-        self.itemimg=itemimage
-        self.defpower=defencepower
-    def setdata(self, name, itemimage, defencepower):
         self.name=name
         self.itemimg=itemimage
         self.defpower=defencepower
@@ -34,62 +26,47 @@ class Sector:
         self.itemsetlist=itemsetlist
 
 class Team:
-    def __init__(self, T_id, position, health, points, attitemlist, defitemlist, defencelist_old, defencelist_new, sleep):
+    def __init__(self, T_id, health, points, attitemlist, defitemlist, sleep):
         self.T_id=T_id
-        self.pos=position
+        self.pos=0 #section no.
         self.hp=health
         self.points=points
         self.attitemlist=attitemlist
         self.defitemlist=defitemlist
-        self.deflist_old=defencelist_old
-        self.deflist_new=defencelist_new
         self.sleep=sleep
-    def setdata(self, T_id, position, health, points, attitemlist, defitemlist, defencelist_old, defencelist_new, sleep):
+
+    def update_health(self, health):
+        self.hp+=health
+
+    def update_point(self, point):
+        self.points+=point
+
+    def add_attitem(self, attitem):
+        self.attitemlist.append(attitem)
+
+    def remove_attitem(self, attitem):
+        self.attitemlist.remove(attitem)
+
+    def add_defitem(self, defitem):
+        self.defitemlist.append(defitem)
+
+    def remove_defitem(self, defitem):
+        self.defitemlist.remove(defitem)
+
+    def setdata(self, health, points, attitemlist, defitemlist, sleep):
         self.T_id=T_id
-        self.pos=position
         self.hp=health
         self.points=points
         self.attitemlist=attitemlist
         self.defitemlist=defitemlist
-        self.deflist_old=defencelist_old
-        self.deflist_new=defencelist_new
         self.sleep=sleep
-team_id=1
-team1=Team(1)
-team2=Team(2)
-team3=Team(3)
-team4=Team(4)
-team5=Team(5)
-team6=Team(6)
-team7=Team(7)
-team8=Team(8)
-team9=Team(9)
-team10=Team(10)
-team11=Team(11)
-team12=Team(12)
-team13=Team(13)
-team14=Team(14)
-team15=Team(15)
-team_list=(team1, team2, team3, team4, team5)
 
 
-nowteam=team1
-sector1=Sector()
-sector2=Sector()
-begining=Attitem()
-jokbo=Defitem()
-
-attacking_team=2
-attacking_item=begining
-defencing_item=jokbo
-howmanyteam=15
-
-
-sectorlist=[sector1, sector2]
 
 def MOVE:
     position=input()
-    pass
+
+
 
 def ATTACK:
     if attacking_team in range(howmanyteam) and attacking_item in nowteam.attitemlist:
