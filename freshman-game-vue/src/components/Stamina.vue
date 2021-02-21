b01263cb1886eadbdc3fd1b1967ad618df6698e3
박찬혁 <chpark1111@naver.com>
2021-02-21 22:01:01 +0900

    range fixed

--- freshman-game-vue/src/components/Stamina.vue
+++ freshman-game-vue/src/components/Stamina.vue
@@ -69,7 +69,7 @@
             const stamina = this.$firebase.database().ref('stamina')
             stamina.on("value", snapshot => {
                 for(let i in snapshot.val()){
-                    this.$set(this.stamina_list, i, snapshot.val()[i]*20)
+                    this.$set(this.stamina_list, i, snapshot.val()[i]*5)
                 }
             })
         },
