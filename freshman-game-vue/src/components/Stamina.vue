90b9165fe20060e78e4339816f5256b5c47761ef
박찬혁 <chpark1111@naver.com>
2021-02-21 22:17:31 +0900

    range fixed

--- freshman-game-vue/src/components/Stamina.vue
+++ freshman-game-vue/src/components/Stamina.vue
@@ -15,7 +15,7 @@
                                 :color="getCol(st(item))"
                                 :value="st(item)"
                                 style="height: 12px; margin-top: 12px; margin-bottom: -4px"
-                                :buffer-value="st(item) === 0 ? 0 : 100"
+                                :buffer-value="st(item) === 0 ? 0 : 200"
                                 stream
                                 rounded
                         />
@@ -30,7 +30,7 @@
                                 :color="getCol(st(item+13))"
                                 :value="st(item+13)"
                                 style="height: 12px; margin-top: 12px; margin-bottom: -4px"
-                                :buffer-value="st(item+13) === 0 ? 0 : 100"
+                                :buffer-value="st(item+13) === 0 ? 0 : 200"
                                 stream
                                 rounded
                         />
