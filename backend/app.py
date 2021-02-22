51983a1b7527efce60819460d78e6a42aee1b3e4
박찬혁 <chpark1111@naver.com>
2021-02-23 02:24:52 +0900

    range fixed

--- backend/app.py
+++ backend/app.py
@@ -428,7 +428,7 @@ def every_second():
             final_rank=[0 for _ in range(n_team+1)]
             sum_rank=[[0,i] for i in range(n_team+1)]
             rk=0
-            for i in range(n_round):
+            for i in range(n_round-1):
                 for j in range(n_team+1):
                     sum_rank[j][0]+=rank_history[-1-i][sum_rank[j][1]]
                 sum_rank.sort()
